5d628723805ca9fc14cc62ee8dd7e282e7f2a664
lukezsmith <lukezachsmith@gmail.com>
2018-12-28 22:20:11 +0000

    final commit

--- build/precache-manifest.276ea12f7ed4f4afe3859fffb2bafed1.js
+++ build/precache-manifest.276ea12f7ed4f4afe3859fffb2bafed1.js
@@ -92,16 +92,16 @@ self.__precacheManifest = [
     "url": "/static/media/brand-icons.a046592b.woff"
   },
   {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
     "url": "/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/static/media/brand-icons.13db00b7.eot"
-  },
-  {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/static/media/icons.8e3c7f55.eot"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
@@ -120,7 +120,7 @@ self.__precacheManifest = [
     "url": "/static/media/brand-icons.a1a749e8.svg"
   },
   {
-    "revision": "2b63c433e057cef34b8912f085436b90",
+    "revision": "b3dc2b846c0c7ca9ec94819e5e28f674",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d32ddec32bbc1d03bb732273671d6e1.js"
+  "/precache-manifest.276ea12f7ed4f4afe3859fffb2bafed1.js"
 );
 
 workbox.clientsClaim();
