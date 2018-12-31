0cc92a0cb7730838a826b929b96a164027ca3706
lukezsmith <lukezachsmith@gmail.com>
2018-12-31 13:31:34 +0000

    backend permissions and auth added, /new/ and /update/ routes removed from frontend

--- lukezsmith/settings.py
+++ lukezsmith/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
         # 'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
