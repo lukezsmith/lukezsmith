aee6b1b582010ec8e16b472373df7b5520bda657
lukezsmith <lukezachsmith@gmail.com>
2019-01-01 13:44:13 +0000

    db test and favicon link change to aws

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b70fc2cec602bb0a66b64ab735e223d.js"
+  "/precache-manifest.8d14adda39f36ae68289ad497857cb37.js"
 );
 
 workbox.clientsClaim();
