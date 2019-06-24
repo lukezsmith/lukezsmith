d88a8f7536c80e28ea08a5605868142bf922a226
lukezsmith <lukezachsmith@gmail.com>
2019-06-24 21:39:36 +0100

    db update

--- lukezsmith/settings.py
+++ lukezsmith/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
