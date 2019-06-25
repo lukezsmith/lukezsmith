4c3a2890fa197b9a5381cadee5b8b1dbd440383d
lukezsmith <lukezachsmith@gmail.com>
2019-06-25 14:54:51 +0100

    admin site url change

--- lukezsmith/urls.py
+++ lukezsmith/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
-    path('admin/', admin.site.urls),
+    path('admin-site/login/', admin.site.urls),
     path('api/', include('blogposts.api.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     re_path('.*', TemplateView.as_view(template_name="index.html")),
