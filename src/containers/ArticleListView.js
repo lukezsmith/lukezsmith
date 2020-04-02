0cbdb12ef46b56ca12bc4ae6d9465b8ef8e73791
lukezsmith <lukezachsmith@gmail.com>
2020-04-02 23:09:48 +0100

    potential article list fix

--- src/containers/ArticleListView.js
+++ src/containers/ArticleListView.js
@@ -8,51 +8,79 @@ import SocialBar from '../components/SocialBar';
 
 class ArticleListView extends React.Component {
 	state = {
+		dataLoaded: false,
 		articles: null
 	};
 
 	async getData() {
 		try {
-			return await axios.get(
-				'https://lukezsmith.herokuapp.com/api-site/blogposts/blogposts/'
-			);
+			await axios
+				.get('https://lukezsmith.herokuapp.com/api-site/blogposts/blogposts/')
+				.then(res => {
+					this.setState({ articles: res });
+				});
 		} catch (error) {
-			console.log(error.message);
+			console.log(`error: ${error.message}`);
 			this.setState({ articles: '404' });
 		}
 		return 0;
 	}
-	async evalData() {
-		const data = await this.getData();
+	// async evalData() {
+	// 	const data = await this.getData();
+	// 	Promise.all(data);
+	// 	console.log(`data: ${data}`);
 
-		if (data !== undefined) {
-			this.setState({
-				articles: data
-			});
-		} else {
-			this.setState({ articles: '404' });
-		}
-	}
+	// 	if (data !== undefined) {
+	// 		// this.setState({
+	// 		// 	articles: data
+	// 		// }
+	// 		// );
+	// 		return data;
+	// 	} else {
+	// 		// this.setState({ articles: '404' });
 
-	componentDidMount() {
-		console.log('mounting');
-		this.evalData();
-		this.render();
+	// 		return '404';
+	// 	}
+	// }
+
+	async componentDidMount() {
+		// console.log('mounting');
+		await this.getData();
+		this.setState({ dataLoaded: true });
+		// Promise.all(([data]),then((res) => {
+		// 	this.setState({ dataLoaded: true });
+		// 	if (res !== undefined){
+		// 		this.setState({ articles: data });
+
+		// 	}else{
+		// 		this.setState({ articles: '404' });
+
+		// 	}
+
+		// );
+
+		// if (data !== undefined) {
+		// 	this.setState({ articles: data });
+		// } else {
+		// 	this.setState({ articles: '404' });
+		// }
 	}
 
 	render() {
-		console.log('rendering');
-		const { articles } = this.state;
+		// console.log('rendering');
+		const { dataLoaded, articles } = this.state;
+		// console.log(dataLoaded);
 
 		let posts = <h1>Oops! No posts yet, please check again later.</h1>;
-		if (articles === null) {
-			console.log('NULL');
+		if (articles === null || dataLoaded == false) {
+			// console.log('NULL');
 			return null;
 		} else {
+			// console.log(articles);
 			const filteredArticles = articles.data.filter(
 				article => article.is_visible === true
 			);
-			console.log(filteredArticles);
+			// console.log(filteredArticles);
 			if (
 				articles.data !== undefined &&
 				articles.data !== '404' &&
