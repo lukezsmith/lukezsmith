69d7ce0b2459c0685c50a16bfaec32d0721a9bcb
lukezsmith <lukezachsmith@gmail.com>
2020-04-02 22:22:52 +0100

    fixed post visibility

--- src/containers/ArticleListView.js
+++ src/containers/ArticleListView.js
@@ -11,56 +11,56 @@ class ArticleListView extends React.Component {
 		articles: null
 	};
 
-	componentDidMount() {
-		const getData = async () => {
-			try {
-				return await axios.get(
-					'https://lukezsmith.herokuapp.com/api-site/blogposts/blogposts/'
-				);
-			} catch (error) {
-				this.setState({ articles: '404' });
-			}
-			return 0;
-		};
-		const evalData = async () => {
-			const data = await getData();
-			if (data !== undefined) {
-				this.setState({ articles: data });
-			} else {
-				this.setState({ articles: '404' });
-			}
-		};
+	async getData() {
+		try {
+			return await axios.get(
+				'https://lukezsmith.herokuapp.com/api-site/blogposts/blogposts/'
+			);
+		} catch (error) {
+			console.log(error.message);
+			this.setState({ articles: '404' });
+		}
+		return 0;
+	}
+	async evalData() {
+		const data = await this.getData();
 
-		evalData();
+		if (data !== undefined) {
+			this.setState({
+				articles: data
+			});
+		} else {
+			this.setState({ articles: '404' });
+		}
+	}
+
+	componentDidMount() {
+		this.evalData();
 	}
 
 	render() {
 		const { articles } = this.state;
-		if (articles === null) {
-			return null;
-		}
-
-		articles.data.forEach((item, index) => {
-			if (item.is_visible === false) {
-				articles.data.splice(index, 1);
-			}
-		});
 
 		let posts = <h1>Oops! No posts yet, please check again later.</h1>;
-
-		if (
-			articles.data !== undefined &&
-			articles.data !== '404' &&
-			articles.data.length !== 0 &&
-			articles.data[0].is_visible !== false
-		) {
-			posts = (
-				<ul id='article-list'>
-					{articles.data.map(article => (
-						<Article data={article} />
-					))}
-				</ul>
+		if (articles === null) {
+			return null;
+		} else {
+			const filteredArticles = articles.data.filter(
+				article => article.is_visible === true
 			);
+			if (
+				articles.data !== undefined &&
+				articles.data !== '404' &&
+				filteredArticles.length !== 0
+			) {
+				posts = (
+					<ul id='article-list'>
+						{filteredArticles.map(article => (
+							<Article data={article} />
+						))}
+					</ul>
+				);
+			}
 		}
 		return (
 			<div className='articleView'>
