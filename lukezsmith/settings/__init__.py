b123b23bc368ab081faa03db11c561a0a6dd5c4a
lukezsmith <lukezachsmith@gmail.com>
2020-10-08 13:00:49 +0100

    test

--- lukezsmith/settings/__init__.py
+++ lukezsmith/settings/__init__.py
@@ -1,32 +1,32 @@
-# # SECURITY WARNING: keep the secret key used in production secret!
-# import json
-# import os
-# # SECRET_KEY = '+2i#a_2ryra6y3_g_6hb9b(w&*xy^axcb2jsh3#b7ppbp*$2ju'
-# SECRET_KEY = os.environ.get('LUKEZSMITH_DJANGO_PASS', 'Not Set')
-# PROD_STATUS = os.environ.get('LUKEZSMITH_DJANGO_PRODSTATUS', "False")
+# SECURITY WARNING: keep the secret key used in production secret!
+import json
+import os
+# SECRET_KEY = '+2i#a_2ryra6y3_g_6hb9b(w&*xy^axcb2jsh3#b7ppbp*$2ju'
+SECRET_KEY = os.environ.get('LUKEZSMITH_DJANGO_PASS', 'Not Set')
+PROD_STATUS = os.environ.get('LUKEZSMITH_DJANGO_PRODSTATUS', "False")
 # SECRET_KEY = os.environ.get('LUKEZSMITH_SECRET_KEY', '')
 
 
-# # CONFIG_FILE = '/etc/config.json'
+# CONFIG_FILE = '/etc/config.json'
 
 
-# if PROD_STATUS == "True":
+if PROD_STATUS == "True":
+    from .production import *
+    # SECRET_KEY = config['SECRET_KEY']
+else:
+    from .development import *
+
+
+
+
+# try:
+#     with open(CONFIG_FILE) as config_file:
+#         config = json.load(config_file)
+#         config['PROD']
 #     from .production import *
-#     # SECRET_KEY = config['SECRET_KEY']
-# else:
+
+
+# except KeyError:
 #     from .development import *
 
-
-
-
-# # try:
-# #     with open(CONFIG_FILE) as config_file:
-# #         config = json.load(config_file)
-# #         config['PROD']
-# #     from .production import *
-
-
-# # except KeyError:
-# #     from .development import *
-
-# # SECRET_KEY = config['SECRET_KEY']
+# SECRET_KEY = config['SECRET_KEY']
