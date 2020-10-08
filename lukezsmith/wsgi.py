0b61f800f93ae198d472ce7fa380635c7b49c384
lukezsmith <lukezachsmith@gmail.com>
2020-10-08 15:04:02 +0100

    test

--- lukezsmith/wsgi.py
+++ lukezsmith/wsgi.py
@@ -17,5 +17,5 @@ from django.core.wsgi import get_wsgi_application
                       
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'lukezsmith.settings')
+                      'settings')
 application = get_wsgi_application()
